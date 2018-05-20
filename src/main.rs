bddacf76159413dc5ec87f153ba2389f3cd83db1
Alexey Voznyuk <me@swizard.info>
2018-05-21 00:46:30 +0300

    some initial interaction code

--- src/main.rs
+++ src/main.rs
@@ -88,11 +88,34 @@ fn run() -> Result<(), Error> {
             error: e,
         }))?;
 
+    let mut env = Env::new();
     while let Some(event) = window.next() {
         let maybe_result = window.draw_2d(&event, |context, g2d| {
             use piston_window::{clear, text, ellipse, line, Transformed};
             // clear everything
             clear([0.0, 0.0, 0.0, 1.0], g2d);
+
+            // draw cursor
+            if let Some((mx, my)) = env.cursor {
+                if let Some((cx, cy)) = env.obj_start {
+                    line([1.0, 0., 0., 1.0], 3., [cx, cy, mx, my], context.transform, g2d);
+                } else {
+                    ellipse(
+                        [1.0, 0., 0., 1.0],
+                        [mx - 5., my - 5., 10., 10.,],
+                        context.transform,
+                        g2d,
+                    );
+                }
+            }
+            // draw menu
+            text::Text::new_color([0.0, 1.0, 0.0, 1.0], 16).draw(
+                &env.business.info_line(),
+                &mut glyphs,
+                &context.draw_state,
+                context.transform.trans(5.0, 20.0),
+                g2d
+            ).map_err(PistonError::DrawText)?;
 
             Ok(())
         });
@@ -103,10 +126,83 @@ fn run() -> Result<(), Error> {
         match event {
             Event::Input(Input::Button(ButtonArgs { button: Button::Keyboard(Key::Q), state: ButtonState::Release, .. })) =>
                 break,
+            Event::Input(Input::Button(ButtonArgs { button: Button::Keyboard(Key::C), state: ButtonState::Release, .. })) =>
+                env.clear(),
+            Event::Input(Input::Move(Motion::MouseCursor(x, y))) =>
+                env.set_cursor(x, y),
+            Event::Input(Input::Cursor(false)) =>
+                env.reset_cursor(),
+            Event::Input(Input::Button(ButtonArgs { button: Button::Mouse(MouseButton::Left), state: ButtonState::Release, .. })) =>
+                env.toggle_obj(),
+            Event::Input(Input::Resize(width, height)) =>
+                env.reset(width, height),
             _ =>
                 (),
         }
     }
 
     Ok(())
+}
+
+enum Business {
+    Construct,
+    Collide,
+}
+
+impl Business {
+    fn info_line(&self) -> String {
+        match self {
+            &Business::Construct =>
+                "[ constructing ] <M> switch to collide mode, <C> to clear or <Q> to exit".to_string(),
+            &Business::Collide =>
+                "[ colliding ] <M> swithc to construct mode, <C> to clear or <Q> to exit".to_string(),
+        }
+    }
+}
+
+struct Env {
+    business: Business,
+    cursor: Option<(f64, f64)>,
+    obj_start: Option<(f64, f64)>,
+}
+
+impl Env {
+    fn new() -> Env {
+        Env {
+            business: Business::Construct,
+            cursor: None,
+            obj_start: None,
+        }
+    }
+
+    fn clear(&mut self) {
+    }
+
+    fn set_cursor(&mut self, x: f64, y: f64) {
+        self.cursor = if y < CONSOLE_HEIGHT as f64 {
+            None
+        } else {
+            Some((x, y))
+        }
+    }
+
+    fn reset_cursor(&mut self) {
+        self.cursor = None;
+        self.obj_start = None;
+    }
+
+    fn toggle_obj(&mut self) {
+        if let Some((mx, my)) = self.cursor {
+            self.obj_start = if let Some((cx, cy)) = self.obj_start {
+                // TODO
+                None
+            } else {
+                Some((mx, my))
+            };
+        }
+    }
+
+    fn reset(&mut self, width: u32, height: u32) {
+        self.reset_cursor();
+    }
 }
